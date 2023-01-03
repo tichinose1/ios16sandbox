bb0aed3344544bfcf7d3ceef7b74441224625253
tichinose1 <tichinose1dev@gmail.com>
2023-01-03 10:28:24 +0900

    Initial Commit

---
+++ ios16sandbox/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ios16sandbox
+//
+//  Created by tichinose1 on 2023/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios16sandbox/ios16sandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios16sandboxApp.swift
+//  ios16sandbox
+//
+//  Created by tichinose1 on 2023/01/03.
+//
+
+import SwiftUI
+
+@main
+struct ios16sandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
