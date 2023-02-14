a89b69ed7672f21c5e1b086fdab5f493904963b1
tichinose1 <tichinose1dev@gmail.com>
2023-02-14 23:59:11 +0900

    Update text

--- ios16sandbox/ContentView.swift
+++ ios16sandbox/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("buta")
+            Text("hoge")
         }
         .padding()
     }
