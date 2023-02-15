1a7d81a25f960d26e0e490be4c8b5635dace06f9
tichinose1 <tichinose1dev@gmail.com>
2023-02-15 23:54:50 +0900

    Add view

---
+++ ios16sandbox/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  ios16sandbox
+//
+//  Created by tichinose1 on 2023/02/15.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
