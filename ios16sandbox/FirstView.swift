1796c47e66445e2976879d715bf9bcb485475578
tichinose1 <tichinose1dev@gmail.com>
2023-02-18 23:55:29 +0900

    Add 2 Views

---
+++ ios16sandbox/FirstView.swift
@@ -0,0 +1,20 @@
+//
+//  FirstView.swift
+//  ios16sandbox
+//
+//  Created by tichinose1 on 2023/02/18.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FirstView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstView()
+    }
+}
---
+++ ios16sandbox/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  ios16sandbox
+//
+//  Created by tichinose1 on 2023/02/18.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
