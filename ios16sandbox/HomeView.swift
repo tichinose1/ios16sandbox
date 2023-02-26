d27fd5bf0ff20087b61109be2f875ff9d1ab1206
tichinose1 <tichinose1dev@gmail.com>
2023-02-26 23:28:57 +0900

    add appearance

--- ios16sandbox/HomeView.swift
+++ ios16sandbox/HomeView.swift
@@ -13,6 +13,10 @@ struct HomeView: View {
         let appearance = UINavigationBarAppearance()
         appearance.backgroundColor = .blue
         UINavigationBar.appearance().scrollEdgeAppearance = appearance
+
+        let appearance2 = UINavigationBarAppearance()
+        appearance2.backgroundColor = .yellow
+        UINavigationBar.appearance().standardAppearance = appearance2
     }
 
     var body: some View {
